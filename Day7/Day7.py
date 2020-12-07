30eade3962d1f57d5158b6488e016b4d3fff8b0f
Kadir Marangoz <k.c.marangoz@student.tue.nl>
2020-12-07 19:03:07 +0100

    day 7 pogggggg

---
+++ Day7/Day7.py
@@ -0,0 +1,36 @@
+#%%
+
+dict_ = {}
+with open("input.txt") as data:
+    for line in data:
+        # print(line)
+        key, value = line[:-2].replace(" bags", "").replace(" bag", "").split(" contain ")
+        # print(value)
+        if "no other" in value:
+            dict_[key] = {}
+            continue
+        other_dict = dict([(val[2:], int(val[0])) for val in value.split(", ")])
+        dict_[key] = other_dict
+
+def find_gold(dict_, colour):
+    if "shiny gold" in dict_[colour]:
+        return True
+    for key in dict_[colour]:
+        if find_gold(dict_, key):
+            return True
+    return False
+
+
+count = 0
+for colour in dict_:
+    if find_gold(dict_, colour):
+        count += 1
+
+
+
+
+
+
+
+
+# %%
